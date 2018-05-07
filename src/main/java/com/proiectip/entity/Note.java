865864df2c6875ad0e4ba7f3c5fc1dcf2b6d44eb
DumitriuAdelina <bzz.adelina@yahoo.com>
2018-05-07 17:03:08 +0300

    note-entity
    note-entity

---
+++ src/main/java/com/proiectip/entity/Note.java
@@ -0,0 +1,28 @@
+package com.proiectip.entity;
+
+public class Note {
+	
+	private int id_user;
+	private int id_produs;
+	private int nota;
+	public int getId_user() {
+		return id_user;
+	}
+	public void setId_user(int id_user) {
+		this.id_user = id_user;
+	}
+	public int getId_produs() {
+		return id_produs;
+	}
+	public void setId_produs(int id_produs) {
+		this.id_produs = id_produs;
+	}
+	public int getNota() {
+		return nota;
+	}
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+
+	
+}
